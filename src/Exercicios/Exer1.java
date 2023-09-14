8cc28cc04a0f29e5e3e3c37c6a296d70180a60f0
Davidbjjj <pondavid103@gmail.com>
2023-09-14 03:12:34 -0300

    Recicly: Exer6 add possibilidades de execução

--- src/Exercicios/Exer1.java
+++ src/Exercicios/Exer1.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Exer1 {
     public static void main(String[] args){
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int a=0;
         System.out.println("Digite um número");
         a=sc.nextInt();
--- src/Exercicios/exer6.java
+++ src/Exercicios/exer6.java
@@ -3,27 +3,29 @@ package Exercicios;
 import java.util.Scanner;
 
 public class exer6 {
-    public static void main(String[] args){
-        int a,b,c=0;
-        Scanner sc= new Scanner(System.in);
+    public static void main(String[] args) {
+        int a, b, c = 0;
+        Scanner sc = new Scanner(System.in);
         System.out.println("digite um número");
-        a=sc.nextInt();
+        a = sc.nextInt();
         System.out.println("digite um número");
-        b=sc.nextInt();
+        b = sc.nextInt();
         System.out.println("digite um número");
-        c=sc.nextInt();
+        c = sc.nextInt();
 
-        if (a>=b && b>=c){
-            System.out.println(c +""+b+""+ a);
+        if (a >= b && b >= c) {
+            System.out.println(c + " " + b + " " + a);
+        } else if (c >= b && b >= a) {
+            System.out.println(a + " " + b + " " + c);
+        } else if (b >= a && a >= c) {
+            System.out.println(c + " " + a + " " + b);
+        } else if (b >= c && c >= a) {
+            System.out.println(a + " " + c + " " + b);
+        } else if (a>c && c>b) {
+            System.out.println(b + " " + c + " " + a);
+        }else if(b>a && a>c) {
+            System.out.println(b + " " + a + " " + c);
         }
-        else if(c>=b && b>=a){
-            System.out.println(a +""+b+""+ c);
-        }
-        else if (b>=a && a>=c){
-            System.out.println(c +""+a+""+ b);
-        }
-        else if(b>=c && c>=a){
-            System.out.println(a +""+c+""+ b);
-        }
+
     }
 }
