471f8df8209038d4d874d926ef4940a669318aef
David Pontes <pondavid103@gmail.com>
2023-11-23 16:06:59 -0300

    add:feat Matrix

--- Javascript/js/at2/variaveis.js
+++ Javascript/js/at2/variaveis.js
@@ -1,15 +1,18 @@
-let sexo="feminino";
-let nome=["Pikachu", "Butterfly", "Cyndaquil"];
-let livel="15";
-let lutar=false;
-let hp=200
+let timePokemon = [
+    ["Pikachu", "M", 200, 18],
+    ["Butterfly", "F", 180, 25],
+    ["Cyndaquil","M", 140, 12]
+]
 
-console.log("o sexo do pokemon é:"+ sexo);
-console.log("o nome do pokemon:"+nome[1]);
+console.log("o nome do pokemon: "+ timePokemon[1][0]);
+console.log("o sexo do pokemon é: "+ timePokemon[1][1]);
 console.log("pode lutar:");
-if(lutar ||  hp<=0){
+
+if(timePokemon[1][2]<=200){
     console.log("pode lutar");
 }else{
     console.log("Não pode lutar");
 }
-console.log("vida:"+hp);
+
+console.log("vida: "+timePokemon[1][2]);
+console.log("Nivel: "+timePokemon[1][3]);
