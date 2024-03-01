16312494430dc246734ce21970e79b440acd8227
David Pontes <60227838+Davidbjjj@users.noreply.github.com>
2024-03-01 20:16:20 -0300

    add:project

---
+++ exer1.js
@@ -0,0 +1,3 @@
+var num1=(prompt("Digite o 1º numero"))
+var num2=(prompt("Digite o 2º numero"))
+var num3=(prompt("Digite o 3º numero"))
---
+++ exer2.js
@@ -0,0 +1,8 @@
+let soma=0;
+var media=0;
+
+var num1=parseInt(prompt("Digite o 1º numero"))
+var num2=parseInt(prompt("Digite o 2º numero"))
+var num3=parseInt(prompt("Digite o 3º numero"))
+soma=num1+num2+num3;
+alert(soma)
