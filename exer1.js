41d56c8cf2df62e00caa311ea973cea5b3fc7cef
David Pontes <pondavid103@gmail.com>
2024-03-02 00:10:01 -0300

    Factored:project

--- exer1.js
+++
@@ -1,3 +0,0 @@
-var num1=(prompt("Digite o 1º numero"))
-var num2=(prompt("Digite o 2º numero"))
-var num3=(prompt("Digite o 3º numero"))
--- exer3.js
+++
@@ -1,4 +0,0 @@
-var num1=(prompt("Digite o 1º numero"))
-var num2=(prompt("Digite o 2º numero"))
-var num3=(prompt("Digite o 3º numero"))
-var exibir=(prompt("Os numero digitados são: "+num1+","+num2+","+num3+","))
